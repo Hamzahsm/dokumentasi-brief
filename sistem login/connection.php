07e1dd0708a9579a44fa8b744bc4c2f991840f55
Hamzahsm <hamzahsaifulmadjid@gmail.com>
2022-02-25 00:11:52 +0700

    sistem login brief secara general

---
+++ sistem login/connection.php
@@ -0,0 +1,3 @@
+<?php 
+// change the values with corret DB
+$con = mysqli_connect('localhost', 'dbuser', 'dbpass', 'dbname');
---
+++ sistem login/new-password.php
@@ -0,0 +1,62 @@
+<?php require_once "controllerUserData.php"; ?>
+<?php 
+$email = $_SESSION['email'];
+if($email == false){
+  header('Location: login-user.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create a New Password</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container"> 
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form">
+                <form action="new-password.php" method="POST" autocomplete="off">
+                    <div style="text-align: center; margin-bottom: 20px;">
+                        <img src="https://oesman.id/wp-content/uploads/2020/08/OESMAN_logo-4.png" width="250px;" height="80px;">
+                    </div>
+                    <h2 class="text-center">New Password</h2>
+                    <?php 
+                    if(isset($_SESSION['info'])){
+                        ?>
+                        <div class="alert alert-success text-center">
+                            <?php echo $_SESSION['info']; ?>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                    <?php
+                    if(count($errors) > 0){
+                        ?>
+                        <div class="alert alert-danger text-center">
+                            <?php
+                            foreach($errors as $showerror){
+                                echo $showerror;
+                            }
+                            ?>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                    <div class="form-group">
+                        <input class="form-control" type="password" name="password" placeholder="Create new password" required>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="password" name="cpassword" placeholder="Confirm your password" required>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control button" type="submit" name="change-password" value="Change">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
